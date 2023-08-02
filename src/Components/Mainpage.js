f03e4fa6923eaf85b857bd77e30e8fc8e3c5243b
eshwar-095 <eshwar095@gmail.com>
2023-08-02 19:21:11 +0530

    react-sass

---
+++ src/Components/Mainpage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/Mainpage.css"
+
+const Mainpage = () => {
+  return (
+    <div className="mainPage">
+       <h1> Welcome to the website </h1>
+        <button>click</button>
+    </div>
+  )
+}
+
+export default Mainpage
